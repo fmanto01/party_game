b54ebba5128bc1de7d9afc88a9c4058e117a2445
pesto13 <ricola1304@gmail.com>
2024-08-04 12:02:37 +0200

    z

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
 import Game from '../public/html/game.jsx';
 import Lobby from '../public/html/lobby.jsx';
 import Home from '../public/html/home.jsx';
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Home />}>
+      <Route path="/lobby" element={<Lobby />} />
+      <Route path="/game" element={<Game />} />
+    </Route>
+  )
+)
+
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/lobby" element={<Lobby />} />
-        <Route path="/game" element={<Game />} />
-      </Routes>
-    </Router>
+    <RouterProvider router={router}></RouterProvider>
   );
 }
 
--- src/server.mjs
+++ src/server.mjs
@@ -5,7 +5,6 @@ import { dirname, join } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { readFile } from 'node:fs/promises';
 
-import { setupRoutes } from './routes.mjs';
 import { setupSocket } from './socket.mjs';
 
 const app = express();
@@ -15,11 +14,13 @@ const io = new Server(server);
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+
+app.use(express.static(join(__dirname, '../public')));
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.use(express.static(join(__dirname, '../public')));
 
 async function init() {
   try {
