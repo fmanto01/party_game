d6c7affe7a682861fc76d8fefeda04d0d47ac2d8
pesto13 <ricola1304@gmail.com>
2024-08-04 14:52:53 +0200

    client sistemato - manca da mettere js

--- client/Scoprimi/src/App.tsx
+++ client/Scoprimi/src/App.tsx
@@ -1,12 +1,23 @@
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import Game from './components/game.tsx';
+import Lobby from './components/lobby.jsx';
+import Home from './components/home.jsx';
 
-
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Home />}>
+      <Route path="/lobby" element={<Lobby />} />
+      <Route path="/game" element={<Game />} />
+    </Route>
+  )
+)
 
 function App() {
-  
-  
+
+
   return (
     <>
-    
+      <RouterProvider router={router}></RouterProvider>
     </>
   )
 }
--- client/Scoprimi/src/main.tsx
+++ client/Scoprimi/src/main.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { io } from 'socket.io-client'
-// import './index.css'
 
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const socket = io('http://localhost:3001');
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
