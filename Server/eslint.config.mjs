77117bee2df2b4cc14a7bc29ee0807966512f5b3
pesto13 <ricola1304@gmail.com>
2024-08-07 15:07:30 +0200

    impostazione quasi ok

--- Server/eslint.config.mjs
+++ Server/eslint.config.mjs
@@ -5,7 +5,7 @@ import stylisticJs from '@stylistic/eslint-plugin-js';
 
 export default [
   { languageOptions: { globals: { ...globals.browser, ...globals.node, io: 'readonly' } } },
-  { ignores: ['dist']},
+  { ignores: ['dist'] },
   {
     plugins: {
       '@stylistic/js': stylisticJs,
