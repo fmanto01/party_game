c29aff5d0070359ad9b1fff60537a7ecfb9321e8
pesto13 <ricola1304@gmail.com>
2024-08-12 22:42:22 +0200

    modifiche stile

--- Scoprimi/src/components/CreateGameForm.tsx
+++ Scoprimi/src/components/CreateGameForm.tsx
@@ -4,23 +4,21 @@ interface CreateGameFormProps {
   onCreateGame: () => void;
 }
 
-const CreateGameForm: React.FC<CreateGameFormProps> = ({ numQuestions, onNumQuestionsChange, onCreateGame }) => {
-  return (
-    <div className="text-center mt-4">
-      <label htmlFor="numQuestions">Numero domande:</label>
-      <input
-        type="number"
-        id="numQuestions"
-        min="5"
-        value={numQuestions}
-        onChange={(e) => onNumQuestionsChange(parseInt(e.target.value))}
-        className="form-control w-25 mx-auto"
-      />
-      <button id="createGameBtn" className="btn btn-primary mt-3" onClick={onCreateGame}>
-        Crea una Partita
-      </button>
-    </div>
-  );
-};
+const CreateGameForm: React.FC<CreateGameFormProps> = ({ numQuestions, onNumQuestionsChange, onCreateGame }) => (
+  <div className="text-center mt-4">
+    <label htmlFor="numQuestions">Numero domande:</label>
+    <input
+      type="number"
+      id="numQuestions"
+      min="5"
+      value={numQuestions}
+      onChange={(e) => onNumQuestionsChange(parseInt(e.target.value))}
+      className="form-control w-25 mx-auto"
+    />
+    <button id="createGameBtn" className="btn btn-primary mt-3" onClick={onCreateGame}>
+      Crea una Partita
+    </button>
+  </div>
+);
 
 export default CreateGameForm;
--- Scoprimi/src/components/LobbyList.tsx
+++ Scoprimi/src/components/LobbyList.tsx
@@ -6,31 +6,29 @@ interface LobbyListProps {
   playerName: string;
 }
 
-const LobbyList: React.FC<LobbyListProps> = ({ lobbies, onJoin, playerName }) => {
-  return (
-    <table id="lobbiesList" className="table table-bordered">
-      <thead>
-        <tr>
-          <th>Codice Lobby</th>
-          <th>Num Giocatori</th>
-          <th>Azioni</th>
+const LobbyList: React.FC<LobbyListProps> = ({ lobbies, onJoin, playerName }) => (
+  <table id="lobbiesList" className="table table-bordered">
+    <thead>
+      <tr>
+        <th>Codice Lobby</th>
+        <th>Num Giocatori</th>
+        <th>Azioni</th>
+      </tr>
+    </thead>
+    <tbody>
+      {lobbies.map((lobby) => (
+        <tr key={lobby.lobbyCode}>
+          <td>{lobby.lobbyCode}</td>
+          <td>{lobby.players.length}</td>
+          <td>
+            <button className="btn btn-success" onClick={() => onJoin(lobby.lobbyCode, playerName)}>
+              Join
+            </button>
+          </td>
         </tr>
-      </thead>
-      <tbody>
-        {lobbies.map((lobby) => (
-          <tr key={lobby.lobbyCode}>
-            <td>{lobby.lobbyCode}</td>
-            <td>{lobby.players.length}</td>
-            <td>
-              <button className="btn btn-success" onClick={() => onJoin(lobby.lobbyCode, playerName)}>
-                Join
-              </button>
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
-};
+      ))}
+    </tbody>
+  </table>
+);
 
 export default LobbyList;
