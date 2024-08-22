60f48d86fd21b876d5e83699dbbfd2ba2b75b2c7
pesto13 <ricola1304@gmail.com>
2024-08-22 13:45:44 +0200

    final

--- Scoprimi/src/App.tsx
+++ Scoprimi/src/App.tsx
@@ -1,32 +1,31 @@
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import Home from './components/home/Home.tsx';
-// import Lobby from './components/lobby/Lobby.tsx';
-// import Game from './components/game/Game.tsx';
-// import { SessionProvider } from './contexts/SessionContext.tsx';
-// import ProtectedRoute from './components/ProtectedRoutes.tsx';
-// import ErrorPage from './components/ErrorPage.tsx';
-// import FinalResults from './components/finalresults/FinalResults.tsx';
-// import Login from './components/login/Login.tsx';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/home/Home.tsx';
+import Lobby from './components/lobby/Lobby.tsx';
+import Game from './components/game/Game.tsx';
+import { SessionProvider } from './contexts/SessionContext.tsx';
+import ProtectedRoute from './components/ProtectedRoutes.tsx';
+import ErrorPage from './components/ErrorPage.tsx';
+import FinalResults from './components/finalresults/FinalResults.tsx';
+import Login from './components/login/Login.tsx';
 
 
 const App = () => (
-  // <SessionProvider>
-  //   <Router>
-  //     <Routes>
-  //       <Route path="/" element={<Home />} />
-  //       <Route path="/login" element={<Login />} />
-  //       <Route
-  //         path="/lobby" element={<ProtectedRoute component={Lobby} />}
-  //       />
-  //       <Route
-  //         path="/game" element={<ProtectedRoute component={Game} />}
-  //       />
-  //       <Route path="/final-results" element={<FinalResults />} />
-  //       <Route path="/error" element={<ErrorPage />} />
-  //     </Routes>
-  //   </Router>
-  // </SessionProvider >
-  <div>porcodio</div>
+  <SessionProvider>
+    <Router basename="/scoprimi">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/lobby" element={<ProtectedRoute component={Lobby} />}
+        />
+        <Route
+          path="/game" element={<ProtectedRoute component={Game} />}
+        />
+        <Route path="/final-results" element={<FinalResults />} />
+        <Route path="/error" element={<ErrorPage />} />
+      </Routes>
+    </Router>
+  </SessionProvider >
 );
 
 export default App;
