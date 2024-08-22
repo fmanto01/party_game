155285fa84efca9a2ad2467c43eaa32f2473f26e
pesto13 <ricola1304@gmail.com>
2024-08-22 16:19:34 +0200

    final

--- Scoprimi/src/ts/socketInit.ts
+++ Scoprimi/src/ts/socketInit.ts
@@ -1,3 +1,8 @@
 import { io } from 'socket.io-client';
-const socket = io('http://localhost:3001');
+
+// const socketUrl = process.env.REACT_APP_SOCKET_URL || 'http://localhost:3001';
+// const socketUrl = 'http://localhost:3001';
+const socketUrl = '44.226.145.213:3001';
+const socket = io(socketUrl);
+
 export { socket };
