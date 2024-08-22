2b0ee32de4a292a8902dc674e6b12fe2fa04c7d5
pesto13 <ricola1304@gmail.com>
2024-08-22 16:26:28 +0200

    cambiato percorsi

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -15,7 +15,7 @@ const server = createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://fmanto01.github.io/party_game'],
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://fmanto01.github.io/'],
 
     methods: ['GET', 'POST'],
   },
