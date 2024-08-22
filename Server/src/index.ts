73e6bf69ac4b75fa3998c64a688c38f76db4d573
pesto13 <ricola1304@gmail.com>
2024-08-22 21:00:23 +0200

    restore

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -13,6 +13,10 @@ app.use(cors<Request>());
 
 const server = createServer(app);
 
+app.get('/test', function (req, res) {
+  res.json({ message: 'Ciao, sono nel server' });
+});
+
 const io = new Server(server, {
   cors: {
     origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://fmanto01.github.io/party_game'],
