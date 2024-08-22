a8988cc350094fbfe11bfa233b4ed6a065049050
pesto13 <ricola1304@gmail.com>
2024-08-22 17:41:06 +0200

    client

--- Scoprimi/src/ts/socketInit.ts
+++ Scoprimi/src/ts/socketInit.ts
@@ -2,7 +2,7 @@ import { io } from 'socket.io-client';
 
 // const socketUrl = process.env.REACT_APP_SOCKET_URL || 'http://localhost:3001';
 // const socketUrl = 'http://localhost:3001';
-const socketUrl = 'https://party-game-backend.onrender.com:3001';
+const socketUrl = 'https://party-game-backend.onrender.com';
 const socket = io(socketUrl);
 
 export { socket };
