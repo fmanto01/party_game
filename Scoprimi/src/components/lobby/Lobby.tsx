81d7edce86d4a4047b2ebc25491f665e4dc548e2
pesto13 <ricola1304@gmail.com>
2024-08-30 01:06:23 +0200

    final

--- Scoprimi/src/components/lobby/Lobby.tsx
+++ Scoprimi/src/components/lobby/Lobby.tsx
@@ -75,25 +75,23 @@ const Lobby: React.FC = () => {
         </div>
         {/* Secondo blocco */}
         <div className="elegant-background mt-3 scrollable fill">
-          <table className="my-table">
-            <tbody>
-              {game.players.map((player) => (
-                <tr key={player}>
-                  <td className="text-center player-image">
-                    <img
-                      src={game.images[player] || 'default-image-url'}
-                      alt={player}
-                      className="player-img" />
-                  </td>
-                  <td className="text-center align-middle player-name">{player}</td>
-                  <td className="text-center align-middle player-status">
-                    <span className={`pill ${game.isReadyToGame[player] ? 'my-bg-success' : 'my-bg-error'}`}>
-                      {game.isReadyToGame[player] ? 'Ready' : 'Not Ready'}
-                    </span>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
+          <table className="my-table my-table-players">
+            {game.players.map((player) => (
+              <tr key={player}>
+                <td className="player-image">
+                  <img
+                    src={game.images[player] || 'default-image-url'}
+                    alt={player}
+                    className="player-img" />
+                </td>
+                <td className="player-name">{player}</td>
+                <td className="player-status">
+                  <span className={`pill ${game.isReadyToGame[player] ? 'my-bg-success' : 'my-bg-error'}`}>
+                    {game.isReadyToGame[player] ? 'Ready' : 'Not Ready'}
+                  </span>
+                </td>
+              </tr>
+            ))}
           </table>
         </div>
         <div className="text-center mt-4">
