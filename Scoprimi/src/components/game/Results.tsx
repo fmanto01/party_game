c9db336d698f3a55b84aa718902713f598cf5576
pesto13 <ricola1304@gmail.com>
2024-08-31 16:49:40 +0200

    sistemato componente front-end

--- Scoprimi/src/components/game/Results.tsx
+++ Scoprimi/src/components/game/Results.tsx
@@ -2,14 +2,22 @@ import React from 'react';
 
 interface ResultsProps {
   resultMessage: string;
+  voteRecap: { [key: string]: string }
 }
 
-const Results: React.FC<ResultsProps> = ({ resultMessage }) => (
+const Results: React.FC<ResultsProps> = ({ resultMessage, voteRecap }) => (
   <div id="resultsContainer" className="text-center mt-3">
     <div id="resultMessageContainer">
-      <p id="resultMessage" dangerouslySetInnerHTML={{ __html: resultMessage }} />
+      <p id="resultMessage">{resultMessage}</p>
+      {
+        Object.entries(voteRecap).map(([voter, vote]) => (
+          <div key={voter}>
+            <strong>{voter}:</strong> {vote}
+          </div>
+        ))
+      }
     </div>
-  </div>
+  </div >
 );
 
 export default Results;
