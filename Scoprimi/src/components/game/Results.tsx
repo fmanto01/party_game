1cdcd0c6159f85e9dab6a6cdb37ceadd74e51070
pesto13 <ricola1304@gmail.com>
2024-09-10 21:21:54 +0200

    client rimosso resultmessage ( old mostVotedPerson version )

--- Scoprimi/src/components/game/Results.tsx
+++ Scoprimi/src/components/game/Results.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 
 interface ResultsProps {
-  resultMessage: string;
   voteRecap: { [key: string]: string };
   playerImages: { [key: string]: string };
   mostVotedPerson: string;
 }
 
-const Results: React.FC<ResultsProps> = ({ resultMessage, voteRecap, playerImages, mostVotedPerson }) => (
+const Results: React.FC<ResultsProps> = ({ voteRecap, playerImages, mostVotedPerson }) => (
   <div id="resultsContainer" className="text-center mt-3">
     <div id="resultMessageContainer">
-      <p id="resultMessage">{resultMessage}</p>
       {
         Object.entries(voteRecap).map(([voter, vote]) => (
           <div key={voter} className="voteEntry">
